8d26d9d16b65e6f8c56b43b175b82df422c43e79
Syed Imran Abidi <imrans110@hotmail.com>
2020-05-02 19:28:26 +0530

    Add Shop Page, Collection Preview & Collection Item Components

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./collection-item.styles.scss";
+
+const CollectionItem = ({ name, imageUrl, price }) => (
+  <div className="collection-item">
+    <div style={{ backgroundImage: `url(${imageUrl})` }} className="image" />
+    <div className="collection-footer">
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
+    </div>
+  </div>
+);
+
+export default CollectionItem;
