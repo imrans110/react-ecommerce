3aec82dccfe8575a6f9359b0c1c7e37e982dac41
Syed Imran Abidi <imrans110@hotmail.com>
2020-05-06 02:48:01 +0530

    Replace all mapStateToProps functions with Reselect Implementations

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
@@ -9,6 +10,7 @@ import Header from "./components/header/header.component";
 import AuthPage from "./pages/auth/auth.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.action";
+import { selectCurrentUser } from "./redux/user/user.selectors";
 class App extends React.Component {
   usubscribedFromAuth = null;
 
@@ -54,8 +56,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser,
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = (dispatch) => ({
