2a3051c30903148ea33c321da5b61a41a88539a2
Syed Imran Abidi <imrans110@hotmail.com>
2020-05-09 21:18:07 +0530

    Add Stripe for Payments

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -9,6 +9,7 @@ import {
 } from "../../redux/cart/cart.selectors";
 
 import "./checkout.styles.scss";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -35,6 +36,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 10/20 - CVV : 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
